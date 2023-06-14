3ff16e5938fb52c508374e4ba2c1b7de0bb8816e
lipinkizkedath <lkizakedath@Tapestry.com>
2023-06-14 09:57:03 +0530

    dashboard added

--- container/src/App.js
+++ container/src/App.js
@@ -1,20 +1,30 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 import Header from './components/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import Progress from './components/Progress';
+import { createBrowserHistory } from 'history';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'cApp',
 });
 
+const history = createBrowserHistory();
+
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <Header
           isSignedIn={isSignedIn}
@@ -25,11 +35,15 @@ function App() {
             <Route path="/auth">
               <AuthLazy onSignIn={() => setIsSignedIn(true)} />
             </Route>
+            <Route path="/dashboard">
+              {!isSignedIn && <Redirect to="/" />}
+              <DashboardLazy />
+            </Route>
             <Route path="/" component={MarketingLazy} />
           </Switch>
         </Suspense>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- dashboard/config/webpack.prod.js
+++ dashboard/config/webpack.prod.js
@@ -3,21 +3,18 @@ const ModuleFederation = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/',
+    publicPath: '/auth/latest/',
   },
   plugins: [
     new ModuleFederation({
-      name: 'container',
-      remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
